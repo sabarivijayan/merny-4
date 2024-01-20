1ff789c2c592e373bbaacec74c5f0258be535683
Sabari Vijayan <sabari25112012@gmail.com>
2024-01-20 22:34:06 +0530

    Initial Commit

---
+++ config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url : 'mongodb+srv://mekryptic:VNQxx6eS5CkIJ32G@cluster0.glr034d.mongodb.net/?retryWrites=true&w=majority'
+};
---
+++ utils/emailValidator.js
@@ -0,0 +1,8 @@
+function validateEmail(email) {
+    const pattern = /^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return pattern.test(email);
+}
+  
+module.exports = {
+    validateEmail : validateEmail,
+}
